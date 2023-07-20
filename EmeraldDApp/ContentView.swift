32ab2b0d3d10ba2d65eb3b7836f393d53db9148b
BoiseITGuru <dev@boiseitguru.com>
2023-07-20 11:45:20 -0600

    Initial Commit

---
+++ EmeraldDApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmeraldDApp
+//
+//  Created by BoiseITGuru on 7/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmeraldDApp/EmeraldDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmeraldDAppApp.swift
+//  EmeraldDApp
+//
+//  Created by BoiseITGuru on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct EmeraldDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
